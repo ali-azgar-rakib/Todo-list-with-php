4f611c5bfc2e11cdb08488c86497c8f2f213d14d
Rakib <crrakib5@gmail.com>
2020-02-29 18:14:59 +0600

    "add a  if statement index_valid.php for avoid to add empty field and add a instruction in readme file"

--- db.php
+++ db.php
@@ -3,9 +3,10 @@ const HOST = 'localhost';
 const USERNAME = 'root';
 const PASSWORD = '';
 const DBNAME = 'todo';
-$dbcon = new mysqli(HOST,USERNAME,PASSWORD,DBNAME);
+$dbcon = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
 
-if($dbcon->connect_error){
-  die("connect error: ".$dbcon->connect_error);
+if ($dbcon->connect_error) {
+  die("connect error: " . $dbcon->connect_error);
 }
+
 ?>
--- index_valid.php
+++ index_valid.php
@@ -2,10 +2,10 @@
 require_once 'db.php';
 if(isset($_POST['addtask'])){
   $task_add = $_POST['textfield'];
-
-$task_add_query = "INSERT INTO task_table (task_name) VALUES ('$task_add')";
-$add_query = $dbcon -> query($task_add_query);
-
+  if(!empty($task_add)){
+    $task_add_query = "INSERT INTO task_table (task_name) VALUES  ('$task_add')";
+    $add_query = $dbcon -> query($task_add_query);
+  }
 
   header('location: index.php');
   
